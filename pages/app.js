e824055fcaf7ac40b4bc1cd039582f252dcf68f0
JM <jdreher15@gmail.com>
2019-12-04 20:32:05 -0800

    stub out next container with apollo wrapper...

---
+++ pages/app.js
@@ -0,0 +1,27 @@
+import App, { Container } from 'next/app';
+import { ApolloProvider, renderToStringWithData } from 'react-apollo';
+
+class MainApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        // this exposes the query to the user
+        pageProps.query = ctx.query;
+        return { pageProps };
+    }
+    render() {
+        const { Component, apollo, pageProps } = this.props;
+
+        return (
+            <Container>
+                <ApolloProvider client={apollo}>
+                    <Page>
+                        <Component {...pageProps} />
+                    </Page>
+                </ApolloProvider>
+            </Container>
+        );
+    }
+}
